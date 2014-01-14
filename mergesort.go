6630a9c8e7a50b895a2c4c807da47c1a95555139
Chris de la Iglesia <cmdli@utexas.edu>
2014-01-13 21:23:31 -0600

    Initial commit

---
+++ mergesort.go
@@ -0,0 +1,68 @@
+//Christopher de la Iglesia
+
+package main
+
+import (
+	"runtime"
+	"fmt"
+	"math"
+	"math/rand"
+)
+
+func main() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	fmt.Println("Testing mergesort...")
+	data := gen_data(100000)
+	//fmt.Println("before:", data)
+	sorted := mergesort(data)
+	//fmt.Println("after:", sorted)
+	err := false
+	for i, val := range sorted {
+		if i != val {
+			err = true
+			fmt.Println("error:",i,"does not equal",val)
+		}
+	}
+	if !err {
+		fmt.Println("pass")
+	}
+	fmt.Println("Done testing mergesort...")
+}
+
+func gen_data(size int) []int {
+	return rand.Perm(size)
+}
+
+func mergesort(arr []int) (out []int) {
+	out = make([]int, len(arr))
+	sorted := make(chan int)
+	go mergesort2(sorted, arr)
+	for i := range out {
+		out[i] = <- sorted
+	}
+	return
+}
+
+func mergesort2(out chan int, arr []int) {
+	if len(arr) <= 1  {
+		out <- arr[0]
+		out <- math.MaxInt32
+		return
+	}
+	a := make(chan int)
+	b := make(chan int)
+	go mergesort2(a, arr[:len(arr)/2])
+	go mergesort2(b, arr[len(arr)/2:])
+	v1 := <- a
+	v2 := <- b
+	for _ = range arr {
+		if v1 < v2 {
+			out <- v1
+			v1 = <- a
+		} else {
+			out <- v2
+			v2 = <- b
+		}
+	}
+	out <- math.MaxInt32
+}
